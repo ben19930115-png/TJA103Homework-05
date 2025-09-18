d0e9dd84195b9c68a8d4d6b74051c7f10d056cd3
Ben Liu <ben19930115@gmail.com>
2025-09-18 18:20:39 +0800

    hw

--- src/hw/hw2.java
+++ src/hw/hw2.java
@@ -26,8 +26,20 @@
 //		}
 //}
 
+// 3.請設計一隻Java程式，計算1～10的連乘積(1*2*3*…*10) (用while迴圈)
+package hw;
 
-
+public class hw2 {
+	public static void main(String[] args) {  
+		int i = 1 ;
+		int sum = 1 ;
+	      while (i <= 10) {
+	    	  sum *= i;
+	          i++;
+	      }
+	    	  System.out.print("1～10的連乘積 = " + sum);
+		}
+}
 
 
 
