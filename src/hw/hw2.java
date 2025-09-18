9c32363d79a929fecd6090beb4d282a325c61aa7
Ben Liu <ben19930115@gmail.com>
2025-09-18 18:12:36 +0800

    hw

--- src/hw/hw2.java
+++ src/hw/hw2.java
@@ -14,18 +14,17 @@
 
 
 // 2.請設計一隻Java程式，計算1～10的連乘積(1*2*3*…*10) (用for迴圈)
-package hw;
-
-public class hw2 {
-	public static void main(String[] args) {  
-		int sum = 1 ;
-	      for (int i = 1; i <= 10; i++) {
-	    	  for (int j = 1; j<= 10;)
-	          sum = i * j;
-	      }
-	    	  System.out.print("1～10的連乘積 = " + sum);
-		}
-}
+//package hw;
+//
+//public class hw2 {
+//	public static void main(String[] args) {  
+//		int sum = 1 ;
+//	      for (int i = 1; i <= 10; i++) {
+//	          sum *= i;
+//	      }
+//	    	  System.out.print("1～10的連乘積 = " + sum);
+//		}
+//}
 
 
 
