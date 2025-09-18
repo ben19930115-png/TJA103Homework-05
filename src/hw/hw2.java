54b36319b82007eb99e76dd60f58baa55e6bee6e
Ben Liu <ben19930115@gmail.com>
2025-09-18 18:01:35 +0800

    hw

--- src/hw/hw2.java
+++ src/hw/hw2.java
@@ -1,19 +1,35 @@
 // 1.請設計一隻Java程式，計算1～1000的偶數和(2+4+6+8+…+1000)
+//package hw;
+//
+//public class hw2 {
+//	public static void main(String[] args) {  
+//		int sum = 0 ;
+//	      for (int i = 2; i <= 1000; i += 2) {
+//	          sum += i;
+//	      }
+//	    	  System.out.print("1～1000的偶數和 = " + sum);
+//		}
+//}
+
+
+
+// 2.請設計一隻Java程式，計算1～10的連乘積(1*2*3*…*10) (用for迴圈)
 package hw;
 
 public class hw2 {
 	public static void main(String[] args) {  
-	      for (int i = 0; i <= 1000; i+=2) {
-	          if (i % 2 == 0) 
-			
-	      System.out.print(i+ " " );
+		int sum = 1 ;
+	      for (int i = 1; i <= 10; i++) {
+	    	  for (int j = 1; j<= 10;)
+	          sum = i * j;
 	      }
-	     
-	      for (int j = 0; j <= 1000; j += 2) {
-	    	  System.out.print(j);
-	      }
-	      
+	    	  System.out.print("1～10的連乘積 = " + sum);
 		}
 }
+
+
+
+
+
 
 
