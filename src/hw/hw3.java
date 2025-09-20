e9656bcf0ab7ce996ce720aff5da8646e32c9c8f
Ben Liu <ben19930115@gmail.com>
2025-09-20 20:40:16 +0800

    hw

--- src/hw/hw3.java
+++ src/hw/hw3.java
@@ -1,9 +1,42 @@
 package hw;
 /*請設計一隻程式，使用者輸入三個數字後，輸出結果會為正三角形、等腰
 三角形、其它三角形或不是三角形，如圖示結果：*/
-public class hw3 {
-	public static void main(String[] args) {
-		
-	}
+import java.util.Scanner;
 
+public class hw3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 輸入三個整數
+        System.out.print("請輸入三個整數: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // 判斷是否能構成三角形 (三角形成立條件)
+        if (a + b > c && a + c > b && b + c > a) {
+            // 正三角形
+            if (a == b && b == c) {
+                System.out.println("正三角形");
+            }
+            // 等腰三角形
+            else if (a == b || b == c || a == c) {
+                System.out.println("等腰三角形");
+            }
+            // 直角三角形 (進階功能)
+            else if (a * a + b * b == c * c ||
+                     a * a + c * c == b * b ||
+                     b * b + c * c == a * a) {
+                System.out.println("直角三角形");
+            }
+            // 其他三角形
+            else {
+                System.out.println("其它三角形");
+            }
+        } else {
+            System.out.println("不是三角形");
+        }
+
+        sc.close();
+    }
 }
