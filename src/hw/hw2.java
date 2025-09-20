85ce69822fe97779dd466e8354c6d02b6b43ba14
Ben Liu <ben19930115@gmail.com>
2025-09-20 18:43:18 +0800

    hw

--- src/hw/hw2.java
+++ src/hw/hw2.java
@@ -24,7 +24,7 @@
 //	      }
 //	    	  System.out.print("1～10的連乘積 = " + sum);
 //		}
-//}
+//} 
 
 // 3.請設計一隻Java程式，計算1～10的連乘積(1*2*3*…*10) (用while迴圈)
 //package hw;
@@ -44,21 +44,34 @@
 /* 4.請設計一隻Java程式，輸出結果為以下：
 1 4 9 16 25 36 49 64 81 100 */
 
+//package hw;
+//
+//public class hw2 {
+//	public static void main(String[] args) {
+//		int sum = 0;   // 用來累加
+//		int  j =  1;   // 從第一個奇數開始
+//	      for (int i = 1; i <= 10; i++) {
+//	    	 sum += j; // 把奇數加進去
+//	      System.out.print(sum +  " ");
+//	      	 j += 2;   // 下一個奇數
+//	      }
+//		}
+//}
+
+/* 5.阿文很熱衷大樂透(1 ～49)，但他不喜歡有4的數字，不論是個位數或是十位數。請設計一隻程式，
+輸出結果為阿文可以選擇的數字有哪些？總共有幾個？*/
+
 package hw;
 
 public class hw2 {
 	public static void main(String[] args) {
-		int sum = 0;   // 用來累加
-		int  j =  1;   // 從第一個奇數開始
-	      for (int i = 1; i <= 10; i++) {
-	    	 sum += j; // 把奇數加進去
-	      System.out.print(sum +  " ");
-	      	 j += 2;   // 下一個奇數
+		int sum = 0;   
+	      for (int i = 1; i <= 49; i++) {
+	    	 if(sum <= 49)
+	    		 sum++ ;
+	      System.out.print(sum +  " ");  
 	      }
 		}
 }
-
-/* 5.阿文很熱衷大樂透(1 ～49)，但他不喜歡有4的數字，不論是個位數或是十位數。請設計一隻程式，
-輸出結果為阿文可以選擇的數字有哪些？總共有幾個？*/
 
 
