86c239a22fcfb7601e43bbc56e15f156c0d444b8
Ben Liu <ben19930115@gmail.com>
2025-09-27 19:32:41 +0800

    hw

--- src/hw/hw3.java
+++ src/hw/hw3.java
@@ -43,29 +43,53 @@ package hw;
 //}
 /*請設計一隻程式，會亂數產生一個0～9的數字，然後可以玩猜數字遊戲，猜錯會顯示錯誤訊息，猜
 對則顯示正確訊息，如圖示結果：*/
+//import java.util.Scanner;
+//
+//public class hw3 {
+//	public static void main(String[] args) {
+//
+//		Scanner scanner = new Scanner(System.in);
+//		int answer = (int)(Math.random() * 10);
+//		int guess;
+//		
+//		System.out.println("猜數字0 ~ 9");
+//		
+//		while (true) {
+//			guess = scanner.nextInt();
+//			if (guess == answer) {
+//				System.out.println("答對了" + answer);
+//				break;
+//			} else {
+//				System.out.println("答錯了");		
+//			}
+//		}
+//		scanner.close();
+//	}			
+//}
+
 import java.util.Scanner;
 
 public class hw3 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		int answer = (int)(Math.random() * 10);
+		int answer = (int)(Math.random() * 100);
 		int guess;
 		
-		System.out.println("猜數字0 ~ 9");
+		System.out.println("猜數字0 ~ 101");
 		
 		while (true) {
 			guess = scanner.nextInt();
-			if (guess == answer) {
-				System.out.println("答對了" + answer);
+			if (guess > answer) {
+				System.out.println("太大了猜小一點");
+			} else if (guess < answer){
+				System.out.println("太小了猜大一點");		
+			}else {
+				System.out.println("答對了");
 				break;
-			} else {
-				System.out.println("答錯了");		
 			}
 		}
 		scanner.close();
 	}			
 }
-
-
 
