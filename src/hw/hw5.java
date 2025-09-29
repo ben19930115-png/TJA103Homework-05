7e5834bb110f7bdb4a4988304d91bb5bc2e5e169
Ben Liu <ben19930115@gmail.com>
2025-09-29 14:52:48 +0800

    hw

--- src/hw/hw5.java
+++ src/hw/hw5.java
@@ -8,16 +8,31 @@ public class hw5 {
 			public static int areaMeasure(int width, int height) {
 				return width * height;	
 			}
+			public static void starSquare(int width, int height) {
+				for (int i = 0; i < height; i++) {
+					for (int j = 0; j < width; j++) {
+						System.out.print("*");
+					}
+					System.out.println();
+			}
+		}
 			public static void main(String args[]) {
 				Scanner sc = new Scanner(System.in);
+				
 				System.out.print("請輸入整數寬與高: ");	
 				int width = sc.nextInt();
 		        int height = sc.nextInt();
 		        
-				int j = areaMeasure(width, height);		
-				System.out.println("長方形面積=" + j);
+				int x = areaMeasure(height, width);		
+				System.out.println("長方形面積=" + x);
+				
+				System.out.println(x);
+				starSquare(width, height);		
 				
 				sc.close();	
-	}
-			
+			}	
 }
+
+
+//請設計一個方法為randAvg()，從10個0～100(含100)的整數亂數中取平均值並印出這10個亂數與平均值，如圖：
+
