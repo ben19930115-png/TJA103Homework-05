c77e0b5425b59caf041a1b1e63584b37d72414e8
Ben Liu <ben19930115@gmail.com>
2025-09-29 14:27:55 +0800

    hw

--- src/hw/hw3.java
+++ src/hw/hw3.java
@@ -1,7 +1,5 @@
 package hw;
 
-import java.util.List;
-
 /*請設計一隻程式，使用者輸入三個數字後，輸出結果會為正三角形、等腰
 三角形、其它三角形或不是三角形，如圖示結果：*/
 //import java.util.Scanner;
--- src/hw/hw5.java
+++ src/hw/hw5.java
@@ -1,5 +1,23 @@
 package hw;
-
+/*請設計一個方法為starSquare(int width, int height)，當使用者鍵盤輸入寬與高時，即會印出對應的*長方形，如
+圖：*/
+import java.util.Scanner;
 public class hw5 {
-
+	
+			// 長方形面積
+			public static int areaMeasure(int width, int height) {
+				return width * height;	
+			}
+			public static void main(String args[]) {
+				Scanner sc = new Scanner(System.in);
+				System.out.print("請輸入整數寬與高: ");	
+				int width = sc.nextInt();
+		        int height = sc.nextInt();
+		        
+				int j = areaMeasure(width, height);		
+				System.out.println("長方形面積=" + j);
+				
+				sc.close();	
+	}
+			
 }
