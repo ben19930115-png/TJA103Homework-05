d1dd17ffc2b64389382369776024ba69317dd59a
Ben Liu <ben19930115@gmail.com>
2026-01-30 17:49:50 +0800

    test

--- src/hw/hw4.java
+++ src/hw/hw4.java
@@ -1,22 +1,31 @@
 package hw;
 
-public class hw4 {
-	public static void main(String[] arg) {
-		int[] intArray = {29,100,39,41,50,8,66,77,95,15};
-		
-		int sum = 0;
-		
-		for(int i = 0;i < intArray.length; i++) {
-			sum += intArray[i];
-		}
-		double avg = (double) sum/intArray.length;
-		System.out.println("元素總合的平均值" + avg);
-				
-		System.out.print("其中大於平均值的元素");
-		for(int i = 0; i < intArray.length; i++) {
-			if(intArray[i] > avg ) {
-				System.out.print(intArray[i] + " ");
-			}
-		}
-	}
-}
+// 有個一維陣列如下：
+//{29, 100, 39, 41, 50, 8, 66, 77, 95, 15}
+//請寫出一隻程式能輸出此陣列所有元素的平均值與大於平均值的元素
+//(提示：陣列，length屬性)
+
+//public class hw4 {
+//	public static void main(String[] arg) {
+//		int[] intArray = {29,100,39,41,50,8,66,77,95,15};
+//		
+//		int sum = 0;
+//		
+//		for(int i = 0;i < intArray.length; i++) {
+//			sum += intArray[i];
+//		}
+//		double avg = (double) sum/intArray.length;
+//		System.out.println("元素總合的平均值" + avg);
+//				
+//		System.out.print("其中大於平均值的元素");
+//		for(int i = 0; i < intArray.length; i++) {
+//			if(intArray[i] > avg ) {
+//				System.out.print(intArray[i] + " ");
+//			}
+//		}
+//	}
+//}
+
+
+
+
