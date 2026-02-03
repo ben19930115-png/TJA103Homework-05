e87618d4e27a8254762ffa977616ef6181850592
Ben Liu <ben19930115@gmail.com>
2026-02-03 13:40:01 +0800

    hw4-2

--- src/hw/hw4.java
+++ src/hw/hw4.java
@@ -1,9 +1,9 @@
 package hw;
 
-// 有個一維陣列如下：
-//{29, 100, 39, 41, 50, 8, 66, 77, 95, 15}
-//請寫出一隻程式能輸出此陣列所有元素的平均值與大於平均值的元素
-//(提示：陣列，length屬性)
+/* 有個一維陣列如下：
+{29, 100, 39, 41, 50, 8, 66, 77, 95, 15}
+請寫出一隻程式能輸出此陣列所有元素的平均值與大於平均值的元素
+(提示：陣列，length屬性)*/
 
 //public class hw4 {
 //	public static void main(String[] arg) {
@@ -25,6 +25,22 @@ package hw;
 //		}
 //	}
 //}
+
+
+/*請建立一個字串，經過程式執行後，輸入結果是反過來的
+例如String s = “Hello World”，執行結果即為dlroW olleH
+(提示：String方法，陣列)*/
+public class hw4{
+	public static void main(String[] arg) {
+		
+		String s = "Hello World";
+		
+		for(int i = s.length() - 1; i >= 0; i--) {
+			System.out.print(s.charAt(i));
+		}
+		
+	}
+}
 
 
 
